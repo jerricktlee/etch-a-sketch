b10b4086cb32908cc94bcc0619112e187ad1b2c6
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-20 20:40:52 -0800

    Add hover eventHandler to change div squares

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,11 @@
 const GRID_SIZE = 16;
+let testVar = 0;
 
+
+let gridSquareList;
 const gridContainer = document.querySelector('.grid-container');
 
 function populateGrid() {
-   console.log("populateGrid() invoked");
    for (let i = 0; i < GRID_SIZE; i++) {
       for (let j = 0; j < GRID_SIZE; j++) {
          let gridSquare = document.createElement('div');
@@ -11,8 +13,14 @@ function populateGrid() {
          gridContainer.appendChild(gridSquare);
       }
    }
+   gridSquareList = document.querySelectorAll('.grid-square');
 }
 
 window.addEventListener('load', () =>{
    populateGrid();
+   gridSquareList.forEach(gridSquare => {
+      gridSquare.addEventListener('mouseenter', (event) => {
+         event.target.style.backgroundColor = 'black';
+      });
+   });
 })
