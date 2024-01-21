f5378a6dd1e61b81d2d0acdfdfd20f339d8fd021
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-20 23:21:31 -0800

    Add functionality to allow user input grid sizes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,14 @@
-const GRID_SIZE = 16;
+const DEFAULT_GRID_SIZE = 16;
 
+let currGridSize = DEFAULT_GRID_SIZE;
 let gridSquareList;
 const gridContainer = document.querySelector('.grid-container');
 const resetBtn = document.querySelector('.reset-btn');
+const customSizeBtn = document.querySelector('.custom-size-btn');
 
-function populateGrid() {
-   for (let i = 0; i < GRID_SIZE; i++) {
-      for (let j = 0; j < GRID_SIZE; j++) {
+function populateGrid(gridSize) {
+   for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
          let gridSquare = document.createElement('div');
          gridSquare.classList.add('grid-square');
          gridContainer.appendChild(gridSquare);
@@ -15,13 +17,17 @@ function populateGrid() {
    gridSquareList = document.querySelectorAll('.grid-square');
 }
 
-window.addEventListener('load', () =>{
-   populateGrid();
-   gridSquareList.forEach(gridSquare => {
+function addEventListenerToSquares(squareNodeList) {
+   squareNodeList.forEach(gridSquare => {
       gridSquare.addEventListener('mouseenter', (event) => {
          event.target.style.backgroundColor = 'black';
       });
    });
+}
+
+window.addEventListener('load', () =>{
+   populateGrid(DEFAULT_GRID_SIZE);
+   addEventListenerToSquares(gridSquareList);
 })
 
 resetBtn.addEventListener('click', () => {
@@ -29,3 +35,22 @@ resetBtn.addEventListener('click', () => {
       gridSquare.style['background-color'] = 'white';
    })
 })
+
+customSizeBtn.addEventListener('click', () => {
+   currGridSize = prompt('Enter a number 1-100, to generate a grid:');
+   if (currGridSize < 1 || currGridSize > 100) {
+      alert('Invald number. The number must be 1-100');
+   }
+   gridSquareList.forEach(gridSquare => {
+      gridSquare.remove();
+   })
+   populateGrid(currGridSize);
+   addEventListenerToSquares(gridSquareList);
+   setCSSGridSize(currGridSize);
+})
+
+function setCSSGridSize(gridDimension) {
+   let squarePercentage = 100 / gridDimension;
+   let root = document.querySelector(':root');
+   root.style.setProperty('--square-size', `${squarePercentage}%`);
+}
